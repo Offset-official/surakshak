30051208752ea91420dd44c0601cec5c3a08a2ad
Anshuman Sharma <69304992+and-human@users.noreply.github.com>
2024-12-31 13:50:50 +0530

    bewakoof me

--- surakshak/models.py
+++ surakshak/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Incidents(models.Model):
+class Incidents_types(models.Model):
 
     INCIDENT_TYPES = {
         "T" : "Tresspassing",
@@ -12,13 +12,12 @@ class Incidents(models.Model):
 
     id = models.AutoField(primary_key = True)
     incident_type = models.CharField(max_length = 1, choices = INCIDENT_TYPES.items())
-    respondent_id = models.ForeignKey('Respondents', on_delete = models.DO_NOTHING)
 
 class Incident(models.Model):
 
     incident_id = models.AutoField(primary_key = True)
     created_at = models.DateTimeField(timezone.now)
-    type = models.ForeignKey('Incidents', on_delete = models.CASCADE)
+    type = models.ForeignKey('Incidents_types', on_delete = models.CASCADE)
     camera_id = models.ForeignKey('Cameras', on_delete = models.CASCADE)
     resolved = models.BooleanField(default = False)
     resolver = models.ForeignKey('Respondents', on_delete = models.CASCADE, null = True)
@@ -27,7 +26,7 @@ class Incident(models.Model):
 class Respondents(models.Model):
 
     id = models.AutoField(primary_key = True)
-    group = models.CharField(max_length = 5)    #define groups later
+    group = models.ForeignKey('Incidents_types', on_delete = models.CASCADE)
     name = models.CharField(max_length = 100)
     phone = models.CharField(max_length = 10)
     email = models.EmailField(max_length = 100)
